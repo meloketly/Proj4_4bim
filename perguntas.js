1e1a43c19a885d81bf5d80de931d07b6e73cd088
meloketly <00001103960726sp@al.educacao.sp.gov.br>
2024-11-25 19:45:03 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Física',
+    'O que é gravitação universal?',
+    'A gravitação universal é a força que atrai objetos com massa uns aos outros, como a Terra e a Lua.'+
+    ' Isaac Newton formulou a lei que descreve essa interação, crucial para a compreensão do movimento dos planetas.'
+)
+
+criaCartao(
+    'Física',
+    'Leis de Newton',
+    'As três leis de Newton explicam o movimento dos corpos, relacionando força, massa e aceleração. São fundamentais para entender fenômenos do dia a dia, como o movimento de um carro ou a queda de um objeto.'
+)
+
+criaCartao(
+    'Física',
+    'O que é eletromagnetismo?',
+    'O eletromagnetismo estuda as interações entre cargas elétricas e campos magnéticos. Ele é essencial para tecnologias como motores elétricos e comunicações via rádio.'
+)
+
+criaCartao(
+    'Física',
+    'Física Quantica',
+    'A física quântica explora o comportamento de partículas em escalas muito pequenas, como átomos e fótons. Contrariando a intuição, ela revela fenômenos como a superposição e o entrelaçamento.'
+)
